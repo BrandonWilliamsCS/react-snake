e360e19980f1d8401454c61f247fcfbc03485702
Brandon.Williams <Brandon.Williams@krush.com>
2017-02-07 13:54:39 -0500

    added keydown listener to change snake direction

--- web-root/src/scripts/Lattice.ts
+++ web-root/src/scripts/Lattice.ts
@@ -1,1 +1,59 @@
+// This is a "string literal" type. It's like an enum, but it's a subtype of string.
 export type Direction = 'up' | 'down' | 'left' | 'right';
+
+export class Cell {
+    readonly x: number;
+    readonly y: number;
+
+    constructor (x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    located(direction: Direction, distance: number = 1): Cell {
+        let newDimension: number;
+        // switch on a string literal type is smart;
+        // it restricts to to the right values, and knows when you've hit every case.
+        switch (direction) {
+        case 'up':
+            newDimension = this.y - distance;
+            return new Cell(this.x, newDimension);
+        case 'down':
+            newDimension = this.y + distance;
+            return new Cell(this.x, newDimension);
+        case 'left':
+            newDimension = this.x - distance;
+            return new Cell(newDimension, this.y);
+        case 'right':
+            newDimension = this.x + distance;
+            return new Cell(newDimension, this.y);
+        }
+    }
+
+    // this return type allows "undefined" for the case where no direction fits.
+    directionTo(otherCell: Cell): Direction | undefined {
+        if (this.x != otherCell.x && this.y != otherCell.y) return undefined;
+
+        if (this.x == otherCell.x) {
+            if (this.y < otherCell.y) {
+                return 'down';
+            } else if (this.y > otherCell.y) {
+                return 'up';
+            } else {
+                return undefined;
+            }
+
+        } else if (this.y == otherCell.y) {
+            if (this.x < otherCell.x) {
+                return 'right';
+            } else if (this.x > otherCell.x) {
+                return 'left';
+            }
+        }
+        return undefined;
+    }
+
+    static equals(left: Cell, right: Cell): boolean {
+        return left.x == right.x && left.y == right.y;
+    }
+}
--- web-root/src/scripts/components/SnakeGame.tsx
+++ web-root/src/scripts/components/SnakeGame.tsx
@@ -11,14 +11,39 @@ interface SnakeGameState {
     direction: Lattice.Direction
 }
 
+// As these aren't being exported, they're not going to clutter up a "global" "namespace".
+const LEFT_KEY_CODE = 37;
+const UP_KEY_CODE = 38;
+const RIGHT_KEY_CODE = 39;
+const DOWN_KEY_CODE = 40;
+
+const LEFT_KEY = 'a';
+const UP_KEY = 'w';
+const RIGHT_KEY = 'd';
+const DOWN_KEY = 's';
+
 export class SnakeGame extends React.Component<SnakeGameOptions, SnakeGameState> {
 
     constructor(props: SnakeGameOptions) {
         super(props);
         this.state = { direction: props.initialDirection }
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
-    render() {
-        return <h1>There is a snake moving {this.state.direction}!</h1>;
+    // events
+    handleKeyDown(evt: React.KeyboardEvent<any>): void {
+        let direction: Lattice.Direction;
+        if (evt.keyCode == UP_KEY_CODE || evt.key == UP_KEY) direction = 'up'
+        else if (evt.keyCode == DOWN_KEY_CODE || evt.key == DOWN_KEY) direction = 'down'
+        else if (evt.keyCode == LEFT_KEY_CODE || evt.key == LEFT_KEY) direction = 'left'
+        else if (evt.keyCode == RIGHT_KEY_CODE || evt.key == RIGHT_KEY) direction = 'right'
+        else return;
+        this.setState({ direction: direction });
+    }
+
+    render(): JSX.Element {
+        return <h1 onKeyDown={this.handleKeyDown} tabIndex="0">There is a snake moving {this.state.direction}!</h1>;
     }
 }
